854a39948199aacf7ff399321dd4ef015532ea5a
yeonjoonhong <yeonjoonh@gmail.com>
2022-05-13 08:25:27 -0700

    blog posting constructor

--- src/main/java/com/example/sping_portfolio/controllers/Greet.java
+++ src/main/java/com/example/sping_portfolio/controllers/Greet.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Greet {
     @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        // @RequestParam handles required and default values, name and blog.model are class variables, blog.model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "greet"; // returns HTML VIEW (greeting)
     }
