4d8ad57250e34af53101832577059dfa79c6e1e6
Ashwin Babu <ashwinbabu888@gmail.com>
2022-05-20 13:45:05 -0700

    remove notifications + blog dropdown in nav

--- src/main/java/com/example/sping_portfolio/controllers/PostsController.java
+++ src/main/java/com/example/sping_portfolio/controllers/PostsController.java
@@ -2,7 +2,6 @@ package com.example.sping_portfolio.controllers;
 
 import com.example.sping_portfolio.models.Post;
 import com.example.sping_portfolio.services.PostService;
-import com.example.sping_portfolio.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,17 +14,9 @@ public class PostsController {
     @Autowired
     private PostService postService;
 
-    @Autowired
-    private NotificationService notifyService;
-
     @RequestMapping("posts/view/{id}")
     public String view(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
-        if (post == null) {
-            notifyService.addErrorMessage("Cannot find post #" + id);
-            return "redirect:/";
-        }
-
         model.addAttribute("post", post);
         return "posts/view";
     }
--- src/main/java/com/example/sping_portfolio/services/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.sping_portfolio.services;
-
-// interface that has all the methods a notification can do
-public interface NotificationService {
-    void addInfoMessage(String msg);
-    void addErrorMessage(String msg);
-}
