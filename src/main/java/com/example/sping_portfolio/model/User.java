3e3ec584ba3f73bf66663388bec20454880d01eb
Ashwin Babu <ashwinbabu888@gmail.com>
2022-05-20 20:59:14 -0700

    add post/user classes

--- src/main/java/com/example/sping_portfolio/model/User.java
+++ src/main/java/com/example/sping_portfolio/model/User.java
@@ -1,5 +1,45 @@
 package com.example.sping_portfolio.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String userName;
+    @Column
+    private String password;
+    @Column
+    private String email;
+
+    // getter methods
+    public Long getId() {
+        return id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    // setter methods
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
