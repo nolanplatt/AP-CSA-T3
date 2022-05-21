bc8df072ec12bd9a4c5bf2dc6c94023471aa6896
Ashwin Babu <ashwinbabu888@gmail.com>
2022-05-20 21:10:12 -0700

    add security config + encryption

---
+++ src/main/java/com/example/sping_portfolio/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.sping_portfolio.controller;
+
+import com.example.sping_portfolio.dto.RegisterRequest;
+import com.example.sping_portfolio.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/sping_portfolio/service/AuthService.java
@@ -0,0 +1,29 @@
+package com.example.sping_portfolio.service;
+
+import com.example.sping_portfolio.dto.RegisterRequest;
+import com.example.sping_portfolio.model.User;
+import com.example.sping_portfolio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+
+        userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
