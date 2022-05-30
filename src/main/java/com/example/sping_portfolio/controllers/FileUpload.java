d5f1c83298a521b534816f72af1c434504cf2ac0
Ashwin Babu <ashwinbabu888@gmail.com>
2022-05-30 16:46:55 -0700

    code handler method for file upload

--- src/main/java/com/example/sping_portfolio/controllers/FileUpload.java
+++ src/main/java/com/example/sping_portfolio/controllers/FileUpload.java
@@ -1,14 +1,46 @@
 package com.example.sping_portfolio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.Date;
+
+import com.example.sping_portfolio.fileupload.Document;
+import com.example.sping_portfolio.fileupload.DocumentRepository;
 
 @Controller // HTTP requests are handled as a controller, using the @Controller annotation
 public class FileUpload {
+
+    @Autowired
+    private DocumentRepository repo;
+
     @GetMapping("/fileupload")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does
-    // variable bindings
     public String fileUpload() {
-        return "/fileupload"; // returns HTML VIEW (greeting)
+        return "/fileupload";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("document") MultipartFile multipartFile, RedirectAttributes ra) throws IOException {
+        
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+        Document document = new Document();
+        document.setName(fileName);
+        document.setContent(multipartFile.getBytes());
+        document.setSize(multipartFile.getSize());
+        document.setUploadTime(new Date());
+
+        repo.save(document);
+
+        ra.addFlashAttribute("message", "The file uploaded successfully!");
+
+        return "redirect:/";
     }
 }
