7f28c9ad37b3faf5a9d68a4ced8733f815d47816
rpeddakama <rishipeddakama@gmail.com>
2022-06-05 23:53:26 -0700

    merge

--- src/main/java/com/example/sping_portfolio/blog/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sping_portfolio.blog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-
-}
